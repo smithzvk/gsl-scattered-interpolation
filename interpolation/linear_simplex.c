656213dd5ba1cd5fbdbbde15b981339a62922e28
Zach Kost-Smith <zachkostsmith@gmail.com>
2015-04-24 23:41:49 -0500

    Move util functions out of main source file.

--- interpolation/linear_simplex.c
+++ interpolation/linear_simplex.c
@@ -12,6 +12,7 @@
 
 #include "linear_simplex.h"
 #include "linear_simplex_integrity_check.h"
+#include "linear_simplex_util.h"
 
 simplex_index
 simplex_tree_node_alloc(simplex_tree *tree)
@@ -475,28 +476,6 @@ insert_point(simplex_tree *tree, simplex_index leaf,
 }
 
 int
-orthogonalize(gsl_matrix *mat)
-{
-  int i;
-  for (i = 0; i < mat->size1 - 1; i++)
-    {
-      gsl_vector_view vi = gsl_matrix_row(mat, i);
-      int j;
-      double vi_mag2 = dnrm22(&(vi.vector));
-      for (j = i+1; j < mat->size1; j++)
-        {
-          gsl_vector_view vj = gsl_matrix_row(mat, j);
-          double proj;
-          gsl_blas_ddot(&(vi.vector), &(vj.vector), &proj);
-          gsl_blas_daxpy(-proj/vi_mag2, &(vi.vector), &(vj.vector));
-        }
-    }
-
-
-  return GSL_SUCCESS;
-}
-
-int
 delaunay(simplex_tree *tree, simplex_index leaf,
          gsl_matrix *data,
          /* face is the index in the simplex that identifies the face */
@@ -783,19 +762,6 @@ delaunay(simplex_tree *tree, simplex_index leaf,
   return ret;
 }
 
-inline double
-dnrm22(gsl_vector *v)
-{
-  int i;
-  double mag2 = 0;
-  for (i = 0; i < v->size; i++)
-    {
-      double val = gsl_vector_get(v, i);
-      mag2 += val*val;
-    }
-
-  return mag2;
-}
 
 int
 in_hypersphere(simplex_tree *tree, simplex_index node,
@@ -838,20 +804,6 @@ in_hypersphere_points(simplex_tree *tree, int *points,
 
   /* Small correction to the radius to remove degenerate cases */
   return dist2 < (r2 * (1 - 10*GSL_DBL_EPSILON));
-}
-
-static int
-singular (const gsl_matrix * LU)
-{
-  size_t i, n = LU->size1;
-
-  for (i = 0; i < n; i++)
-    {
-      double u = gsl_matrix_get (LU, i, i);
-      if (u == 0) return 1;
-    }
-
-  return 0;
 }
 
 /* See http://steve.hollasch.net/cgindex/geometry/sphere4pts.html (in

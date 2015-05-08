48ca7d0bf6ef453ab96817a7223dae9fa50bd07f
Zach Kost-Smith <zachkostsmith@gmail.com>
2015-05-08 18:48:01 -0500

    Indent fix

--- interpolation/linear_simplex_integrity_check.c
+++ interpolation/linear_simplex_integrity_check.c
@@ -139,7 +139,7 @@ _check_delaunay(simplex_tree *tree, simplex_index node)
   double r2;
   int *points = tree->tmp_points1;
   for (i = 0; i < tree->dim+1; i++)
-      points[i] = POINT(node, i);
+    points[i] = POINT(node, i);
   calculate_hypersphere_points(tree, points, gdata, x0, &r2, tree->accel);
 
   /* Check every point to see if any lie within the sphere. */
